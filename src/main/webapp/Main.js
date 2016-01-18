ccea48a20562b41984bad55b6cfab17acfe5acd3
Eva Zuka <eva.zhuk@gmail.com>
2016-01-18 18:35:59 +0200

    Main.js has been added

---
+++ src/main/webapp/Main.js
@@ -0,0 +1,45 @@
+/**
+ * Created by Eva on 18.01.2016..
+ */
+
+Ext.define('BookCatalog.view.Main', {
+    extend: 'Ext.container.Container',
+
+    requires: [
+        'BookCatalog.view.MainController',
+        'BookCatalog.view.MainModel'
+    ],
+
+    xtype: 'app-main',
+
+    controller: 'main',
+    plugins: 'viewport',
+    viewModel: {
+        type: 'main'
+    },
+
+    layout: {
+        type: 'border'
+    },
+    items: [{
+        xtype: 'panel',
+        bind: {
+            title: '{name}'
+        },
+        region: 'west',
+        html: '<ul><li>This area is commonly used for navigation, for example, using a "tree" component.</li></ul>',
+        width: 250,
+        split: true,
+        tbar: [{
+            text: 'Button',
+            handler: 'onClickButton'
+        }]
+    },{
+        region: 'center',
+        xtype: 'tabpanel',
+        items:[{
+            title: 'Tab 1',
+            html: '<h2>Content appropriate for the current navigation.</h2>'
+        }]
+    }]
+});
